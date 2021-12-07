eb98699f2a86a421f19e3a5c793747578939aaac
unknown <draganas03@gmail.com>
2021-12-07 19:34:20 +0100

    css styling

--- src/components/Card/CardComponent.js
+++ src/components/Card/CardComponent.js
@@ -2,21 +2,16 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
-const CardComponent = () => {
+const CardComponent = ({ imgSrc, title, text }) => {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={imgSrc} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
